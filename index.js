bf4f77cb11e4a64a58640ca7b0da353fe9f76c07
Alexr42 <alexr42.radu@gmail.com>
2023-12-14 04:22:50 +0100

    index.js; ej24

--- index.js
+++ index.js
@@ -92,9 +92,17 @@ let valorMultiplicacion = MiNumeroFav * variableValorNumerico;
 let valorDivision = MiNumeroFav / 3;
 
 //BUCLES
-
 //24. - Crear variable contarHasta10 con valor 0 e incrementar su valor con un bucle while hasta que se verifique que contarHasta10 === 10
+let contarHasta10 = 0
+while (contarHasta10 < 10){
+    contarHasta10++;
+}
 
 //25. - Crear las variables preI y preJ con valor 0 a continuación cree un bucle que itere 11 veces.En cada iteración se deberá sumar al valor de preI el valor de++preJ
+let preI = 0
+let preJ = 0
+for (i = 0; i < 11; i++){
+    preI += ++preJ;
+};
 
 //26. - Crear la variable sumaImpares con valor 0 a continuación crea un bucle que itere 10 veces(i < 10) si la iteración es impar se deberá sumar a sumaImpares el número de la iteración actual(i)
