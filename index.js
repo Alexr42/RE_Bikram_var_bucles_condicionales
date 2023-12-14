b62e7f905bc50f23a2c7d23194017d0c8cd9882a
Alexr42 <alexr42.radu@gmail.com>
2023-12-14 01:59:33 +0100

    index.js; ej3

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ let variableSinValor;
 //2. - Crear 2 variables tipo let de nombres booleano1 y booleano2 con valores booleanos
 let booleano1 = true;
 let booleano2 = false;
+
 //3. - Crear variable tipo const de nombre PI declarada con valor 3.14
+const PI = 3.14;
 
 //4. - Crear variable tipo const de nombre TAU declarada con valor 2 veces PI
 
